362cf921fda4349a2de197f5ab373eb7ca7607f9
LukeParkerDev <10430890+Hona@users.noreply.github.com>
2023-12-28 21:56:41 +1000

    parallel ingest zone records

--- API/src/TempusHub.API/Features/Ingests/Services/Tasks/IngestMapRecordsTask.cs
+++ API/src/TempusHub.API/Features/Ingests/Services/Tasks/IngestMapRecordsTask.cs
@@ -1,6 +1,7 @@
 ﻿using EFCore.BulkExtensions;
 using TempusApi;
 using TempusApi.Enums;
+using TempusApi.Models.Activity;
 using TempusApi.Models.DetailedMapList;
 using TempusHub.API.Features.ZoneRecords;
 
@@ -9,67 +10,72 @@ namespace TempusHub.API.Features.Ingests.Services.Tasks;
 public class IngestMapRecordsTask(ITempusClient tempusClient, ILogger<IngestMapRecordsTask> logger, AppDbContext dbContext) : IIngestTask
 {
     public async Task ExecuteAsync(Ingest ingest, DateOnly triggerDate, CancellationToken cancellationToken = default)
-    {
-        var allMaps = await tempusClient.GetDetailedMapListAsync(cancellationToken);
-
-        var zoneTypes = new[]
         {
-            ZoneType.Map,
-            ZoneType.Course,
-            ZoneType.Bonus,
-            ZoneType.Trick
-        };
-        
-        logger.LogInformation("IngestMapRecords: Will require {ApiRequestCount} API requests", 
-            1 + allMaps
-                .SelectMany(x => zoneTypes
-                    .Select(zt => GetZoneCount(zt, x.ZoneCounts)))
-                .Sum());
+            var allMaps = await tempusClient.GetDetailedMapListAsync(cancellationToken);
 
-        await using var transaction = await dbContext.Database.BeginTransactionAsync(cancellationToken);
-
-        for (var mapIndex = 0; mapIndex < allMaps.Count; mapIndex++)
-        {
-            var map = allMaps[mapIndex];
-            
-            logger.LogInformation("IngestMapRecords: Processing map {MapName} ({MapIndex}/{MapCount})",
-                map.Name, mapIndex + 1, allMaps.Count);
-            
-            foreach (var zoneType in zoneTypes)
+            var zoneTypes = new[]
             {
-                for (var zoneIndex = 1; zoneIndex <= GetZoneCount(zoneType, map.ZoneCounts); zoneIndex++)
+                ZoneType.Map,
+                ZoneType.Course,
+                ZoneType.Bonus,
+                ZoneType.Trick
+            };
+
+            logger.LogInformation("IngestMapRecords: Will require {ApiRequestCount} API requests",
+                1 + allMaps
+                    .SelectMany(x => zoneTypes
+                        .Select(zt => GetZoneCount(zt, x.ZoneCounts)))
+                    .Sum());
+
+            await using var transaction = await dbContext.Database.BeginTransactionAsync(cancellationToken);
+
+            int batchSize = 5;
+
+            foreach (var batch in allMaps.Chunk(5))
+            {
+                var tasks = batch.Select(map => FetchMapRecords(map, zoneTypes, tempusClient, triggerDate, cancellationToken)).ToArray();
+                var batchResults = await Task.WhenAll(tasks);
+                
+                var allFetchedRecords = batchResults.SelectMany(x => x).ToList();
+                if (allFetchedRecords.Count != 0)
                 {
-                    var allZoneRecords = await tempusClient.GetTopZonedTimes(map.Id, zoneType, zoneIndex, limit: 0,
-                        cancellationToken: cancellationToken);
-
-                    logger.LogInformation("Got {Count} records for {MapName} {ZoneType} {ZoneIndex}",
-                        allZoneRecords.Runs.DemomanRuns.Count + allZoneRecords.Runs.SoldierRuns.Count, map.Name,
-                        zoneType, zoneIndex);
-
-                    // Null defense for concat
-                    allZoneRecords.Runs.DemomanRuns ??= [];
-                    allZoneRecords.Runs.SoldierRuns ??= [];
-
-                    // Combine solly & demo runs into 1 enumerable, so only 1 projection is needed
-                    var allRecords = allZoneRecords.Runs.DemomanRuns
-                        .Concat(allZoneRecords.Runs.SoldierRuns)
-                        .Select(x => new ZoneRecord()
-                        {
-                            Id = x.Id,
-                            ZoneId = x.ZoneId,
-                            Date = TempusUtility.GetDateFromTimestamp(x.Date),
-                            Class = x.Class,
-                            Duration = TimeSpan.FromSeconds(x.Duration),
-                            PlayerId = x.UserId,
-                            ArchivedDate = triggerDate
-                        });
-
-                    await dbContext.BulkInsertAsync(allRecords, cancellationToken: cancellationToken);
+                    await dbContext.BulkInsertAsync(allFetchedRecords, cancellationToken: cancellationToken);
                 }
+            }
+
+            await transaction.CommitAsync(cancellationToken);
+        }
+
+    private async Task<IEnumerable<ZoneRecord>> FetchMapRecords(DetailedMapOverviewModel map, ZoneType[] zoneTypes, ITempusClient tempusClient, DateOnly triggerDate, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("IngestMapRecords: Fetching records for map {MapName}", map.Name);
+        var allRecords = new List<ZoneRecord>();
+
+        foreach (var zoneType in zoneTypes)
+        {
+            for (var zoneIndex = 1; zoneIndex <= GetZoneCount(zoneType, map.ZoneCounts); zoneIndex++)
+            {
+                var allZoneRecords = await tempusClient.GetTopZonedTimes(map.Id, zoneType, zoneIndex, limit: 0, cancellationToken: cancellationToken);
+
+                allZoneRecords.Runs.DemomanRuns ??= new List<RecordInfoShort>();
+                allZoneRecords.Runs.SoldierRuns ??= new List<RecordInfoShort>();
+
+                allRecords.AddRange(allZoneRecords.Runs.DemomanRuns
+                    .Concat(allZoneRecords.Runs.SoldierRuns)
+                    .Select(x => new ZoneRecord()
+                    {
+                        Id = x.Id,
+                        ZoneId = x.ZoneId,
+                        Date = TempusUtility.GetDateFromTimestamp(x.Date),
+                        Class = x.Class,
+                        Duration = TimeSpan.FromSeconds(x.Duration),
+                        PlayerId = x.UserId,
+                        ArchivedDate = triggerDate
+                    }));
             }
         }
 
-        await transaction.CommitAsync(cancellationToken);
+        return allRecords;
     }
 
     private static int GetZoneCount(ZoneType zoneType, ZoneCounts mapZoneCounts) =>
