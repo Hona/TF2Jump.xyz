9f0c1108aaf3a18babb5bdd5a8032ba123a07a59
LukeParkerDev <10430890+Hona@users.noreply.github.com>
2024-07-03 14:05:12 +1000

    updates, work with map page filter

---
+++ UI/src/TF2Jump.WebUI/TF2Jump.WebUI.Client/Utilities/TF2JumpExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace TF2Jump.WebUI.Client.Utilities;
+
+public static class TF2JumpExtensions
+{
+    public const string MapClassesUrl =
+        "https://raw.githubusercontent.com/Hona/TF2Jump.xyz/feat-indended-classes/Utilities/TF2Jump.Utilities.IntendedMapList/MapClasses.csv";
+
+    
+    public static async Task<Dictionary<string, IntendedClasses>> GetIntendedClassMapListAsync(this HttpClient client)
+    {
+        var text = await client.GetStringAsync(MapClassesUrl);
+        
+        return text
+            .Split('\n')
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .Select(x => x.Split(','))
+            .ToDictionary(x => x[1], x => x[0] switch
+            {
+                "S" => IntendedClasses.Soldier,
+                "D" => IntendedClasses.Demoman,
+                _ => IntendedClasses.Demoman | IntendedClasses.Soldier
+            });
+    }
+}
+    
+[Flags]
+public enum IntendedClasses
+{
+    Soldier,
+    Demoman
+}
--- Utilities/TF2Jump.Utilities.IntendedMapList/Program.cs
+++ Utilities/TF2Jump.Utilities.IntendedMapList/Program.cs
@@ -20,12 +20,6 @@ var missingMaps = maps.Select(x => x.Name)
     .Except(intendedMapClasses.Select(x => x.Map))
     .OrderBy(x => x)
     .ToList();
-
-var invalidMaps = intendedMapClasses
-    .Where(x => !maps.Select(y => y.Name).Contains(x.Map))
-    .OrderBy(x => x.Map)
-    .ToList();
-
 if (missingMaps.Any())
 {
     Console.WriteLine("Missing maps:");
@@ -35,12 +29,32 @@ if (missingMaps.Any())
     }
 }
 
+var invalidMaps = intendedMapClasses
+    .Where(x => !maps.Select(y => y.Name).Contains(x.Map))
+    .OrderBy(x => x.Map)
+    .ToList();
 if (invalidMaps.Any())
 {
     Console.WriteLine("Invalid maps:");
     foreach (var invalidMap in invalidMaps)
     {
         Console.WriteLine(invalidMap.Map);
+    }
+}
+
+var duplicateMaps = intendedMapClasses
+    .GroupBy(x => x.Map)
+    .Where(x => x.Count() > 1)
+    .Select(x => x.Key)
+    .OrderBy(x => x)
+    .ToList();
+
+if (duplicateMaps.Any())
+{
+    Console.WriteLine("Duplicate maps:");
+    foreach (var duplicateMap in duplicateMaps)
+    {
+        Console.WriteLine(duplicateMap);
     }
 }
 
