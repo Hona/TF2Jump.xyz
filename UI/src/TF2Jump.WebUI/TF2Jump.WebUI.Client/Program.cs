32f9f7b0214dca5a82191242cdef18885e975f06
Luke Parker <10430890+Hona@users.noreply.github.com>
2024-07-04 18:46:34 +1000

    💄 Basic Home page + Top Players Online (naive) (#61)
    * 💄 Basic Home page + Top Players Online (naive)
    
    * persist prerendered state

--- UI/src/TF2Jump.WebUI/TF2Jump.WebUI.Client/Program.cs
+++ UI/src/TF2Jump.WebUI/TF2Jump.WebUI.Client/Program.cs
@@ -3,6 +3,7 @@ using Humanizer.Configuration;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using TempusApi;
+using TF2Jump.WebUI.Client.Services;
 using TF2Jump.WebUI.Utilities.Humanizer;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -13,5 +14,6 @@ builder.Services.AddFluentUIComponents();
 
 builder.Services.AddHttpClient<ITempusClient, TempusClient>();
 builder.Services.AddSingleton<ITempusClient, TempusClient>();
+builder.Services.AddSingleton<ServerResolver>();
 
 await builder.Build().RunAsync();
